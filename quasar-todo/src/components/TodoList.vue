95845e9e78d4fc619e18d11ab41e389396a8bbc5
Henrik Au-Young <auhenrik@gmail.com>
2025-07-14 12:10:52 +0800

    TodoList component added and working

---
+++ quasar-todo/src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="q-pa-md">
+    <q-list bordered separator>
+      <q-item v-for="entry in entries" :key="entry.id">
+        <q-item-section>
+          <q-item-label> {{ entry.name }} </q-item-label>
+          <q-item-label caption lines="2"> {{ entry.description }} </q-item-label>
+        </q-item-section>
+        <q-item-section side top>
+          <q-item-label caption> {{ entry.category }} </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
+
+<script setup lang="ts">
+  export type Entry = {
+    id: string;
+    name: string;
+    description: string;
+    category: 'chores' | 'work' | 'exercise' | null;
+  };
+
+  defineProps<{ entries: Entry[] }>()
+</script>
--- quasar-todo/src/pages/TodoPage.vue
+++ quasar-todo/src/pages/TodoPage.vue
@@ -1,43 +1,40 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page>
+    <TodoList :entries="entries" />
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
+  import { ref } from 'vue'
+  import TodoList from 'components/TodoList.vue'
+  import type { Entry } from 'components/TodoList.vue'
 
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+  const entries = ref<Entry[]>([
+    {
+      id: 'id1',
+      name: '1',
+      description: 'asd',
+      category: 'chores'
+    },
+    {
+      id: 'id2',
+      name: '2',
+      description: 'sadw',
+      category: null
+    },
+    {
+      id: 'id3',
+      name: '3',
+      description: 'asd',
+      category: 'work'
+    },
+    {
+      id: 'id4',
+      name: '4',
+      description: 's',
+      category: 'exercise'
+    }
+  ])
+
 </script>
